5c3f8f62368041a21c4b77f35be46a8886f05203
Dvonderstueck <dennis.vonderstueck@grandcentrix.net>
2023-08-11 15:49:27 +0200

    more_updates

--- oop_exercise.py
+++ oop_exercise.py
@@ -1,25 +1,57 @@
+
 class info:
     def __init__(self, name, email, address):
          self.name = name
          self.email = email
          self.address = address
 
+
+
+
 class Student(info):
     def __init__(self, name, email, address):
         super().__init__(name, email, address)
+        
+
+        self.student_grades = {}  # Ein leeres Dictionary zur Speicherung der Noten.
+
+    def add_grade(self, discipline, grade):
+        self.student_grades[discipline] = grade # Füge die Note zur entsprechenden Disziplin hinzu.
 
     def get_full_info(self):
      return f"Name: {self.name}\nEmail: {self.email}\nAddress: {self.address}"
+    
+
+class Discipline:
+    def __init__(self, name):
+        self.name = name
+
+
+class add_question:
+    def __init__(self, question):
+        self.exam_questions.append(question)
+
 
 class Teacher(info):
     def __init__(self, name, email, address):
         super().__init__(name, email, address)
+        self.taught_disciplines = [] # Eine Liste der unterrichteten Disziplinen.
+        self.exam_questions = []  # Eine Liste von Prüfungsfragen
+
+    def teach_discipline(self, discipline):
+        self.taught_disciplines.append(discipline) # Füge die unterrichtete Disziplin hinzu.
+
+
+    def question(self, question_text):
+        self.exam_questions.append(question_text) # Füge eine Prüfungsfrage hinzu.
+
 
     def get_full_info(self):
-     return f"Name: {self.name}\nEmail: {self.email}\nAddress: {self.address}"
+        return f"Name: {self.name}\nEmail: {self.email}\nAddress: {self.address}"
 
-class Director(info):
-  assigned_schools = {}
+
+class Director(Teacher):
+  assigned_schools = {} # Ein Dictionary zur Zuordnung von Schulen zu Schulleitern.
 
   def __init__(self, name, email, address, school_name):
         super().__init__(name, email, address)
@@ -27,7 +59,7 @@ class Director(info):
             raise ValueError(f"{school_name} already has a director.")
         self.school_name = school_name
         Director.assigned_schools[school_name] = self
-        self.school_assigned = False
+        self.school_assigned = False # Zeigt an, ob eine Schule zugewiesen wurde.
         
 
   def assign_school(self, school_name):
@@ -41,12 +73,17 @@ class Director(info):
   def get_full_info(self):
       return f"Name: {self.name}\nEmail: {self.email}\nAddress: {self.address}\nSchool: {self.school_name}"
 
+
 class Secretary(info):
     def __init__(self, name, email, address):
         super().__init__(name, email, address)
 
     def get_full_info(self):
      return f"Name: {self.name}\nEmail: {self.email}\nAddress: {self.address}"
+    
+math_discipline = Discipline("Mathematics")
+physics_discipline = Discipline("Physics")
+english_discipline = Discipline("English")
 
 Student1 = Student("Dennis", "dennis.vonderstueck@grandcentrix.net", "boissereestraße 5 köln")
 Teacher1 = Teacher("Müller", "müller@school.com", "Newroad 16 köln")
@@ -57,7 +94,14 @@ Director2 = Director("Smith", "Smith@school.com", "Smithstreet 6 köln", "Evt")
 Director1.assign_school("Europaschule köln")
 Director2.assign_school("Evt")
 
-print(Director2.get_full_info())
+Teacher1.teach_discipline(math_discipline.name,)
+Teacher1.teach_discipline(physics_discipline.name)
+
+Teacher1.question("what is 1+1")
+Student1.add_grade(math_discipline.name, 85)
+
+##print("Student1 has the grade: ",Student1.student_grades, "by the Teacher", Teacher1.name)
+
 
 
 
