f08366be38b9c1e61191325eaaf71536e14a5291
Dvonderstueck <dennis.vonderstueck@grandcentrix.net>
2023-09-11 15:38:35 +0200

    Doku

--- Student_class.py
+++ Student_class.py
@@ -1,29 +1,98 @@
+# Import necessary class from corresponding module
 from info_class import Info
 
+# Define a Student class that inherits from the Info class
 class Student(Info):
 
     def __init__(self, name, email, address):
+        """
+        Initialize a Student object.
+
+        Args:
+            name (str): The name of the student.
+            email (str): The email address of the student.
+            address (str): The address of the student.
+
+        Attributes:
+            student_grades (dict): A dictionary to store the student's grades for different disciplines.
+            assigned_classes (list): A list to store the classes assigned to the student.
+
+        """
         super().__init__(name, email, address)
         self.student_grades = {}
         self.assigned_classes = []
 
     def add_grade(self, discipline, grade):
+        """
+        Add a grade for a specific discipline to the student's record.
+
+        Args:
+            discipline (str): The name of the discipline.
+            grade (float): The grade received for the discipline.
+
+        """
         self.student_grades[discipline] = grade
 
     @staticmethod
     def get_input(question):
+        """
+        Get user input.
+
+        Args:
+            question (str): The question to display to the user.
+
+        Returns:
+            str: The user's input as a string.
+
+        """
         return input(question)
 
     def get_final_grade(self, discipline_name):
+        """
+        Get the final grade for a specific discipline.
+
+        Args:
+            discipline_name (str): The name of the discipline.
+
+        Returns:
+            float or None: The final grade for the specified discipline, or None if not found.
+
+        """
         return self.student_grades.get(discipline_name)
 
     def add_final_grade(self, discipline_name, final_grade):
+        """
+        Add a final grade for a specific discipline to the student's record.
+
+        Args:
+            discipline_name (str): The name of the discipline.
+            final_grade (float): The final grade for the discipline.
+
+        """
         self.student_grades[discipline_name] = final_grade
 
     def get_exam_grades(self, discipline_name):
+        """
+        Get the exam grades for a specific discipline.
+
+        Args:
+            discipline_name (str): The name of the discipline.
+
+        Returns:
+            float or None: The exam grades for the specified discipline, or None if not found.
+
+        """
         return self.student_grades.get(discipline_name)
 
     def save_points_to_file(self, discipline, exam_name):
+        """
+        Save the student's points for a specific discipline and exam to a file.
+
+        Args:
+            discipline (str): The name of the discipline.
+            exam_name (str): The name of the exam.
+
+        """
         file_path = f"{self.name}_{discipline}_{exam_name}_saved_points.txt"
 
         new_points = self.student_grades.get(discipline, 0)
@@ -32,3 +101,5 @@ class Student(Info):
             file.write(f"Saved points for {discipline}: {new_points}\n")
 
         print(f"Total points for {discipline} in {exam_name} have been updated and saved to {file_path}")
+
+    print(save_points_to_file.__doc__)
--- Teacher_class.py
+++ Teacher_class.py
@@ -1,26 +1,56 @@
+# Import necessary classes from corresponding modules
 from info_class import Info
 from Math_class import Math
 from English_class import English
 from Physics_class import Physics
 from Adress_class import Adress
 
-
+# Create an instance of the Address class
 address_instance = Adress()
 
+# Create instances of exam lists for different disciplines
 math_exam_list = Math("Mathematics")  
 english_exam_list = English("English")
 physics_exam_list = Physics("Physics")
 
+# Define a Teacher class that inherits from the Info class
 class Teacher(Info):
     
     def __init__(self, name, email, address):
+        """
+        Initialize a Teacher object.
+
+        Args:
+            name (str): The name of the teacher.
+            email (str): The email address of the teacher.
+            address (str): The address of the teacher.
+
+        Attributes:
+            taught_disciplines (list): A list to store the disciplines taught by the teacher.
+
+        """
         super().__init__(name, email, address)
         self.taught_disciplines = []
 
     def teach_discipline(self, discipline1, discipline2):
+        """
+        Assign two disciplines to be taught by the teacher.
+
+        Args:
+            discipline1 (object): The first discipline to be taught.
+            discipline2 (object): The second discipline to be taught.
+
+        """
         self.taught_disciplines.extend([discipline1, discipline2])
 
     def exam_quest_list_teacher(self, disciplines):
+        """
+        Print exam questions for the disciplines taught by the teacher.
+
+        Args:
+            disciplines (list): A list of disciplines to retrieve exam questions for.
+
+        """
         for discipline in disciplines:
             exams = discipline.conduct_exams()
             for i, exam in enumerate(exams, start=1):
@@ -31,16 +61,23 @@ class Teacher(Info):
                 print()
 
     def list_teacher_disciplines(self):
+        """
+        Print the disciplines taught by the teacher.
+
+        """
         print(f"{self.name}'s Taught Disciplines:")
         for discipline in self.taught_disciplines:
             print(discipline.name)
 
+# Create instances of Teacher objects with random addresses
 math_teacher = Teacher("Lukas", "Lukas@school.com", address_instance.generate_random_address())
 english_teacher = Teacher("Nina", "Nina@school.com", address_instance.generate_random_address())
 physics_teacher = Teacher("Tommy", "Tommy@school.com", address_instance.generate_random_address())
 
+# Assign disciplines to be taught by each teacher
 math_teacher.teach_discipline(math_exam_list, physics_exam_list)
 english_teacher.teach_discipline(english_exam_list, math_exam_list)
 physics_teacher.teach_discipline(physics_exam_list, physics_exam_list)
 
+# Print an empty line for separation
 print()
